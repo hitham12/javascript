2d156def493f4db3a228050bca3fe598391c019a
hitham12 <89399236+hitham12@users.noreply.github.com>
2021-10-09 22:44:54 +0300

    Delete 4.5.js

--- Different Ways to Declare Functions/4.5.js
+++
@@ -1,9 +0,0 @@
-function leapYear(year){
-    return((year % 4 == 0) && (year % 100 !=0)) || (year % 4 == 0);
-
-}
-    
-console.log(leapYear(2012));
-console.log(leapYear(2001));
-console.log(leapYear(2008));
-
