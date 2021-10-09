e002caf6d4e948b085ddc81aeab562aff00186b6
hitham12 <89399236+hitham12@users.noreply.github.com>
2021-10-09 22:45:12 +0300

    Delete 2.2.js

--- Function Basics/2.2.js
+++
@@ -1,9 +0,0 @@
-function tellfortune(jobTitle, location, Partnername, numberofchildren)
-{
-
-return `you will be a${jobTitle} in ${location} and married to ${Partnername} with ${numberofchildren} childrens`;
-
-
-}
-
-console.log(tellfortune('programer', 'haifa', 'gal',"2"));
