130c751bfd8679ecf3e8567544481c2e472f2030
hitham12 <89399236+hitham12@users.noreply.github.com>
2021-10-09 22:40:10 +0300

    Create 2.2.js

---
+++ Function Basics/2.2.js
@@ -0,0 +1,9 @@
+function tellfortune(jobTitle, location, Partnername, numberofchildren)
+{
+
+return `you will be a${jobTitle} in ${location} and married to ${Partnername} with ${numberofchildren} childrens`;
+
+
+}
+
+console.log(tellfortune('programer', 'haifa', 'gal',"2"));
